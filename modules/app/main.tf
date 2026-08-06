910ed213382f1c62fa3d1a32bc911e947cf0e4e5
Kah <kah@kah.com>
2026-08-06 04:17:46 +0000

    fix: add required deployment and pod template metadata blocks

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -1,20 +1,48 @@
-
 resource "kubernetes_deployment" "tasky" {
+  metadata {
+    name = "tasky-deployment"
+    labels = {
+      app = "tasky"
+      env = var.environment
+    }
+  }
+
   spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "tasky"
+      }
+    }
+
     template {
+      metadata {
+        labels = {
+          app = "tasky"
+          env = var.environment
+        }
+      }
+
       spec {
         container {
           name  = "tasky"
-          image = var.app_image  # Dynamically receives image from CI/CD
-          
-          # Force K8s to pull fresh builds
+          image = var.app_image
+
           image_pull_policy = "Always"
 
-          port { container_port = 8080 }
+          port {
+            container_port = 8080
+          }
 
           env {
             name  = "MONGODB_URI"
             value = "mongodb://taskyUser:Tasky123@${var.mongo_ip}:27017/go-mongodb?authSource=admin"
+          }
+
+          env {
+            name  = "SECRET_KEY"
+            value = "wiz-secret-key"
           }
         }
       }
